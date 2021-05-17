377111ec42006ddd4253c5bd089fc80ca6051aa5
Anshul1310 <huntinggamingtech@gmail.com>
2021-05-17 11:07:50 +0530

    SignUp UI created

--- app/src/main/java/com/anstudios/indiclearn/EmailLogin.java
+++ app/src/main/java/com/anstudios/indiclearn/EmailLogin.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
+public class EmailLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_email_login);
     }
 
-    public void email_login(View view) {
-        startActivity(new Intent(RegisterActivity.this,EmailLogin.class));
-        overridePendingTransition(R.anim.slide_next,R.anim.slide_down);
+    public void register_edit_profile(View view) {
     }
+
+
 }
